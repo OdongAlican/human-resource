214a8f3da36c3862cad88469dfe11e89520d3d47
Odong Sunday <sandieo.2020@gmail.com>
2024-01-31 22:26:20 +0300

    test save permission

--- src/main/java/com/humanresource/hr/permission/Permission.java
+++ src/main/java/com/humanresource/hr/permission/Permission.java
@@ -6,9 +6,7 @@ import com.humanresource.hr.role.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -17,6 +15,8 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "permissions")
 public class Permission {
     @Id
---
+++ src/test/java/com/humanresource/hr/repository/PermissionRepositoryTests.java
@@ -0,0 +1,40 @@
+package com.humanresource.hr.repository;
+
+import com.humanresource.hr.permission.Permission;
+import com.humanresource.hr.permission.PermissionRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PermissionRepositoryTests {
+
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    @Test
+    @DirtiesContext
+    public void testSavePermission() {
+        Permission permission = createPermission();
+        Permission data = permissionRepository.save(permission);
+
+        Permission response = permissionRepository.findById(data.getId()).orElse(null);
+
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.getName()).isEqualTo("create");
+        Assertions.assertThat(response.getName()).isNotEqualTo("update");
+
+    }
+
+
+    private Permission createPermission() {
+        return Permission.builder()
+                .name("create")
+                .build();
+    }
+}
