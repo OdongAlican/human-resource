56a19d40ad76a225d6de7773474bec42ca5fb1b1
Odong Sunday <sandieo.2020@gmail.com>
2024-02-01 21:17:01 +0300

    test save role

---
+++ src/test/java/com/humanresource/hr/repository/RoleRepositoryTests.java
@@ -0,0 +1,38 @@
+package com.humanresource.hr.repository;
+
+import com.humanresource.hr.role.Role;
+import com.humanresource.hr.role.RoleRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class RoleRepositoryTests {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Test
+    @DirtiesContext
+    public void testSaveRole() {
+        Role role = createRole();
+        Role response = roleRepository.save(role);
+
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.getName()).isEqualTo("Admin");
+        Assertions.assertThat(response.getName()).isNotEqualTo("Accountant");
+    }
+
+    @Test
+    @DirtiesContext
+    public void testUpdateRole(){
+
+    }
+
+    private Role createRole() {
+        return Role.builder().name("Admin").build();
+    }
+}
