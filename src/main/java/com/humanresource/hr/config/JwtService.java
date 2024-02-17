46bcb856185c2175b384aa3271cde9c47d5908a6
Odong Sunday <sandieo.2020@gmail.com>
2024-02-17 11:56:42 +0300

    update jwt service

--- src/main/java/com/humanresource/hr/config/JwtService.java
+++ src/main/java/com/humanresource/hr/config/JwtService.java
@@ -1,10 +1,33 @@
 package com.humanresource.hr.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "22cf33e41f2c8d2daa16881a4197a0c051070724a2d6df2944c67896ba9a697d";
+
     public String extractUserEmail(String token) {
         return null;
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
