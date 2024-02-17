158c5b8a0164a8e5bfca28733b06949c05e91426
Odong Sunday <sandieo.2020@gmail.com>
2024-02-17 22:41:22 +0300

    add application configs

--- src/main/java/com/humanresource/hr/config/JwtService.java
+++ src/main/java/com/humanresource/hr/config/JwtService.java
@@ -46,6 +46,19 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String userEmail = extractUserEmail(token);
+        return (userEmail.equals(userDetails.getUsername())) && isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     public Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
